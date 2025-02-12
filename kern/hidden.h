ca1f1f2f8441ec316abbc9bbde946b94223c0ccf
Aravind Machiry <amachiry@purdue.edu>
2025-02-12 16:05:29 -0500

    Adding lab 3

--- kern/hidden.h
+++ kern/hidden.h
@@ -1,10 +1,8 @@
-#include <inc/x86.h>
-#include <inc/mmu.h>
-#include <inc/error.h>
+#include "inc/stdio.h"
+#include <inc/types.h>
 #include <inc/string.h>
-#include <inc/assert.h>
+#include "inc/types.h"
+#include <inc/elf.h>
+#include "inc/stdio.h"
 
-#include <kern/pmap.h>
-#include <kern/kclock.h>
-
-void hidden_test_cases();
+bool hidden_test_cases(); 
